2080f4723aee2b007edca3660ebba37b541b8a7b
dishantm24 <dishantmohite24@gmail.com>
2023-05-26 13:28:03 +0530

    push

--- jobcreationand execution.sql
+++ jobcreationand execution.sql
@@ -2,23 +2,126 @@ USE msdb;
 GO
 
 EXEC dbo.sp_add_job
-    @job_name = 'ExportDataToCSV1',
+    @job_name = 'ImportDatafromcsv',
     @enabled = 1;
 GO
 
 EXEC msdb.dbo.sp_add_jobstep
-    @job_name = 'ExportDataToCSV1',
-    @step_name = 'ExportStep3',
+    @job_name = 'ImportDatafromcsv',
+    @step_name = 'marginreport',
     @subsystem = 'CMDEXEC',
-    @command = 'bcp "SELECT ISINNO FROM marginreport" queryout "D:\FirstAssignment\opfile.csv" -c -t, -T -S VSL1184',
-    @on_success_action = 3;
+    @command = 'bcp venturaDb..marginreport in "D:\FirstAssignment\marginreport.csv" -c -t, -T -S VSL1184',
+    @on_success_action = 1;
+GO
+
+EXEC msdb.dbo.sp_add_jobstep
+    @job_name = 'ImportDatafromcsv',
+    @step_name = 'bhav1',
+    @subsystem = 'CMDEXEC',
+    @command = 'bcp venturaDb..bhav1 in "D:\FirstAssignment\bhav1.csv" -c -t, -T -S VSL1184',
+    @on_success_action = 1;
+GO
+
+EXEC msdb.dbo.sp_add_jobstep
+    @job_name = 'ImportDatafromcsv',
+    @step_name = 'bhav2',
+    @subsystem = 'CMDEXEC',
+    @command = 'bcp venturaDb..bhav2 in "D:\FirstAssignment\bhav2.csv" -c -t, -T -S VSL1184',
+    @on_success_action = 1;
+GO
+
+EXEC msdb.dbo.sp_add_jobstep
+    @job_name = 'ImportDatafromcsv',
+    @step_name = 'bhav3',
+    @subsystem = 'CMDEXEC',
+    @command = 'bcp venturaDb..bhav3 in "D:\FirstAssignment\bhav3.csv" -c -t, -T -S VSL1184',
+    @on_success_action = 1;
+GO
+
+EXEC msdb.dbo.sp_add_jobstep
+    @job_name = 'ImportDatafromcsv',
+    @step_name = 'bhav4',
+    @subsystem = 'CMDEXEC',
+    @command = 'bcp venturaDb..bhav4 in "D:\FirstAssignment\bhav4.csv" -c -t, -T -S VSL1184',
+    @on_success_action = 1;
+GO
+
+EXEC msdb.dbo.sp_add_jobstep
+    @job_name = 'ImportDatafromcsv',
+    @step_name = 'bhav5',
+    @subsystem = 'CMDEXEC',
+    @command = 'bcp venturaDb..bhav5 in "D:\FirstAssignment\bhav5.csv" -c -t, -T -S VSL1184',
+    @on_success_action =1;
+GO
+
+EXEC msdb.dbo.sp_add_jobstep
+    @job_name = 'ImportDatafromcsv',
+    @step_name = 'NSEM',
+    @subsystem = 'CMDEXEC',
+    @command = 'bcp venturaDb..NSEM in "D:\FirstAssignment\NSEM.csv" -c -t, -T -S VSL1184',
+    @on_success_action =1;
 GO
 
 
+EXEC msdb.dbo.sp_add_jobstep
+    @job_name = 'ImportDatafromcsv',
+    @step_name = 'BSEM',
+    @subsystem = 'CMDEXEC',
+    @command = 'bcp venturaDb..BSEM in "D:\FirstAssignment\BSEM.csv" -c -t, -T -S VSL1184',
+    @on_success_action =1;
+GO
+
+
+EXEC msdb.dbo.sp_add_jobstep
+    @job_name = 'ImportDatafromcsv',
+    @step_name = 'Security',
+    @subsystem = 'CMDEXEC',
+    @command = 'bcp venturaDb..securitydata in "D:\FirstAssignment\security.csv" -c -t, -T -S VSL1184',
+    @on_success_action =1;
+GO
+
+
+EXEC msdb.dbo.sp_add_jobstep
+    @job_name = 'ImportDatafromcsv',
+    @step_name = 'VAR',
+    @subsystem = 'CMDEXEC',
+    @command = 'bcp venturaDb..VARreport in "D:\FirstAssignment\VAR.csv" -c -t, -T -S VSL1184',
+    @on_success_action =1;
+GO
+
+
+EXEC msdb.dbo.sp_add_jobstep
+    @job_name = 'ImportDatafromcsv',
+    @step_name = 'SPAN',
+    @subsystem = 'CMDEXEC',
+    @command = 'bcp venturaDb..spanreport in "D:\FirstAssignment\span.csv" -c -t, -T -S VSL1184',
+    @on_success_action =1;
+GO
+
+--exec xp_cmdshell
+--'bcp venturaDb..BSEM format nul -n -f  "D:\FirstAssignment\BSEM.txt" -c -T -S VSL1184'
+
+
+BULK insert venturaDb..bhav1 from 'D:\FirstAssignment\6\cm17MAY2023bhav.xlsx'
+with(FIELDTERMINATOR =',',ROWTERMINATOR ='\n')
+
+
 USE MSDB
-EXEC sp_add_jobserver @job_name = 'ExportDataToCSV1', @server_name =
+EXEC sp_add_jobserver @job_name = 'ImportDatafromcsv', @server_name =
 'VSL1184'
 
+USE venturaDb
+EXEC TruncateTablesscriptcategory_SP
+Go
+
 USE MSDB
-EXEC dbo.sp_start_job @job_name = 'ExportDataToCSV1';
+EXEC dbo.sp_start_job @job_name = 'ImportDatafromcsv';
+
 GO
+
+
+USE venturaDb
+EXEC ScriptCateogryRecord_SP
+Go
+
+drop table bhav1
--- new_category_final.sql
+++ new_category_final.sql
@@ -51,34 +51,36 @@
 --if EXISTS (select  * from  #temp1)
 --BEGIN
 --drop table #temp1;
-
 DROP TABLE	IF EXISTS #temp1;
 
 
 with
 varvalues as (
-select   FNOIND, m1.[ scripcode], case 
-when tf.FNOIND  ='IND' then ceiling((vr.VAR + vr.ELM*3 + vr.ADDI) + 5  )
-when tf.FNOIND  ='FNO' then ceiling((vr.VAR + vr.ELM*3 + vr.ADDI) + 5  )
+select  FNOIND, m1.[ scripcode],
+case 
+when tf.FNOIND  ='IND' then ceiling((vr.VAR + (vr.ELM*3) + vr.ADDI) + 5  )
+when tf.FNOIND  ='FNO' then ceiling((vr.VAR + (vr.ELM*3) + vr.ADDI) + 5  )
 when tf.FNOIND is null  AND( m1.[ Category] in ('A','B','Q')
- OR m1.[ Category] like 'A%'  OR m1.[ Category] like 'B%' 
- OR m1.[ Category] like 'Q%')
-then ceiling ( (vr.VAR + vr.ELM*5 + vr.ADDI) + 5 )
-when tf.FNOIND is null  and m1.[ Category] in ('C','D','E')
- OR m1.[ Category] like 'C%'  
-then ceiling ( (vr.VAR + vr.ELM + vr.ADDI) + 5 )
-else ceiling (( vr.VAR + vr.ELM + vr.ADDI)+5)
-end as 'var+el+adii' from marginreport m1 
+OR m1.[ Category] like 'A%'  
+OR m1.[ Category] like 'B%' 
+OR m1.[ Category] like 'Q%') 
+then ceiling ( (vr.VAR + (vr.ELM*5)+ vr.ADDI) + 5 )
+when tf.FNOIND is null  and( m1.[ Category] in ('C','D','E')
+OR m1.[ Category] like 'C%') 
+then ceiling ( (vr.VAR + (vr.ELM) + vr.ADDI) + 5 )
+else (vr.VAR +vr.ELM+vr.ADDI)
+end as 'var+el+adii'
+from marginreport m1 
 left   join tokeninfo tf on m1.[ scripcode] = tf.TOKEN 
 inner  join [dbo].[NSEM] ns on m1.[ scripcode] = ns.Token
 inner join VARreport vr on m1.ISINNO = vr.ISINNO
- where ns.series not in ('BE','BO','BZ','BT','IT','SM','SG','ST','SZ') 
- --AND m1.ISINNO = 'INE002L01015'
- group by FNOIND,m1.[ scripcode],VAR,ELM,ADDI, m1.[ Category]
+where ns.series not in ('BE','BO','BZ','BT','IT','SM','SG','ST','SZ') 
+ --AND m1.ISINNO = 'INE256H01015'
+group by FNOIND,m1.[ scripcode],VAR,ELM,ADDI, m1.[ Category]
 ),
 
- fnovalues as ( select   m2.[ scripcode],(sp.[SPANMgn%] +sp.[ExpMgn%]+sp.[AddExpMgn%]) *100 as 'FNOMAR'
- from marginreport m2
+fnovalues as ( select   m2.[ scripcode],(sp.[SPANMgn%] +sp.[ExpMgn%]+sp.[AddExpMgn%]) *100 as 'FNOMAR'
+from marginreport m2
 left   join tokeninfo tf on m2.[ scripcode] = tf.TOKEN 
 inner  join [dbo].[NSEM] ns on m2.[ scripcode] = ns.Token
 left join spanreport sp on m2.[ Scrip_Name] = sp.Symbol
@@ -91,23 +93,31 @@ select   m3.[ scripcode],FNOIND,
 case when 
 FNOIND = 'IND' then 25 when 
 FNOIND = 'FNO' then 30 
-else 38
-end as 'maxvalues'
+when FNOIND is NULL AND m3.[ Category] in ('A','B','Q') OR m3.[ Category] like 'A%'   OR m3.[ Category] like 'B%'   OR m3.[ Category] like 'Q%' then 38 
+when tf.FNOIND is null and m3.[ Category] ='C' then 50
+when tf.FNOIND is null and  m3.[ Category] = 'C10' then 60
+when tf.FNOIND is null and  m3.[ Category] = 'C20' then 70
+when tf.FNOIND is null and  m3.[ Category] = 'C25' then 75
+when tf.FNOIND is null and  m3.[ Category] = 'C30' then 80 
+when tf.FNOIND is null and  m3.[ Category] = 'C49' then 99
+when tf.FNOIND IS NULL and m3.[ Category] ='D'  then 100
+when tf.FNOIND IS NULL and m3.[ Category] ='E'  then 100
+end as 'maxvalues'	   
 from marginreport m3 
 left   join tokeninfo tf on m3.[ scripcode] = tf.TOKEN 
 inner  join [dbo].[NSEM] ns on m3.[ scripcode] = ns.Token
 where ns.series not in ('BE','BZ','BT','BO','IT','SM','SG','ST','SZ')
-
-group by m3.[ scripcode],FNOIND
+--and ISINNO = 'INE985W01018'
+group by m3.[ scripcode],FNOIND,m3.[ Category]
 
 ),
 
 imvalue as (select  m4.[ scripcode],
 Case
-when m4.[ Category] = 'A' and m4.[ Category] like 'A%' then (vr.VAR + vr.ELM + 5) 
-when m4.[ Category] = 'B' and m4.[ Category] like 'B%' then (vr.VAR + vr.ELM + 5) 
-when m4.[ Category] = 'Q' and m4.[ Category] like 'Q%' then (vr.VAR + vr.ELM + 5) 
-when m4.[ Category] = 'C' and m4.[ Category] like 'C%' then (vr.VAR + vr.ELM + vr.ADDI+  5)
+when m4.[ Category] = 'A' OR m4.[ Category] like 'A%' then (vr.VAR + vr.ELM + 5) 
+when m4.[ Category] = 'B' OR m4.[ Category] like 'B%' then (vr.VAR + vr.ELM + 5) 
+when m4.[ Category] = 'Q' OR m4.[ Category] like 'Q%' then (vr.VAR + vr.ELM + 5) 
+when m4.[ Category] = 'C' OR m4.[ Category] like 'C%' then (vr.VAR + vr.ELM + vr.ADDI+  5)
 when m4.[ Category] = 'D'  then (vr.VAR + vr.ELM + vr.ADDI+  5)
 when m4.[ Category] = 'E'  then (vr.VAR + vr.ELM + vr.ADDI+  5)
 else (vr.VAR + vr.ELM + vr.ADDI+  5)
@@ -115,11 +125,11 @@ end as 'I_M'
 from marginreport m4
 inner  join [dbo].[NSEM] ns on m4.[ scripcode] = ns.Token
 inner join VARreport vr on m4.ISINNO = vr.ISINNO
- where ns.series not in ('BE','BZ','BT','BO','IT','SM','SG','ST','SZ') 
- group by m4.[ scripcode],m4.[ Category],VAR,ELM,ADDI
+where ns.series not in ('BE','BZ','BT','BO','IT','SM','SG','ST','SZ') 
+group by m4.[ scripcode],m4.[ Category],VAR,ELM,ADDI
 
  ),
-   maxcatnew as (
+maxcatnew as (
 select   m5.[ scripcode],FNOIND,
 case when 
 FNOIND = 'IND' then 20  when FNOIND= 'FNO' then 25
@@ -159,7 +169,6 @@ b5.[CLOSE] as 'TRate',
 round ((abs((b5.[CLOSE]-b4.[CLOSE])/b5.[CLOSE]))* 100 ,3 ) as '1Day', --ABS((P1-O1)/P1)*100
 round ((abs((b5.[CLOSE]-b3.[CLOSE])/b5.[CLOSE]))* 100 , 3 ) as '2Day', --ABS((P1-N1)/P1)*100
 case 
---when ns.Series not in ('GS','GB') then (b1.TOTTRDQTY+b2.TOTTRDQTY+b3.TOTTRDQTY+b4.TOTTRDQTY+b5.TOTTRDQTY)/5
 when  b1.TOTTRDQTY <> 0 OR  b1.TOTTRDQTY = 0  then   (b1.TOTTRDQTY+b2.TOTTRDQTY+b3.TOTTRDQTY+b4.TOTTRDQTY+b5.TOTTRDQTY)/5
 when  b2.TOTTRDQTY <> 0 OR  b2.TOTTRDQTY = 0  then (b1.TOTTRDQTY+b2.TOTTRDQTY+b3.TOTTRDQTY+b4.TOTTRDQTY+b5.TOTTRDQTY)/5
 when  b3.TOTTRDQTY <> 0 OR  b3.TOTTRDQTY = 0  then  (b1.TOTTRDQTY+b2.TOTTRDQTY+b3.TOTTRDQTY+b4.TOTTRDQTY+b5.TOTTRDQTY)/5
@@ -175,26 +184,32 @@ sp.[SPANMgn%] ,
 sp.[ExpMgn%],
 sp.[AddExpMgn%],
 f.FNOMAR,--=(Y1+Z1+AA1)*100
-case when v.[var+el+adii] > mx.maxvalues and v.[var+el+adii] > f.FNOMAR then v.[var+el+adii]
-when mx.maxvalues> v.[var+el+adii] and mx.maxvalues > f.FNOMAR then mx.maxvalues
-when  f.FNOMAR > v.[var+el+adii]and  f.FNOMAR >mx.maxvalues then  f.FNOMAR
-when m.[ Category] ='C' and m.[ Category] like 'C%' then 50
-when m.[ Category] ='D' and m.[ Category] like 'D%' then 100
-when m.[ Category] ='E' and m.[ Category] like 'E%' then 100
-else mx.maxvalues
+case when v.[var+el+adii] > mx.maxvalues then v.[var+el+adii]
+when mx.maxvalues> f.FNOMAR then mx.maxvalues
+when tf.FNOIND is NULL AND( m.[ Category] in ('A','B','Q')
+OR m.[ Category] like 'A%'  
+OR m.[ Category] like 'B%' 
+ OR m.[ Category] like 'Q%') then   38
+ when tf.FNOIND is null and m.[ Category] ='C' then 50
+when tf.FNOIND is null and  m.[ Category] = 'C10' then 60
+when tf.FNOIND is null and  m.[ Category] = 'C20' then 70
+when tf.FNOIND is null and  m.[ Category] = 'C25' then 75
+when tf.FNOIND is null and  m.[ Category] = 'C30' then 80 
+when tf.FNOIND is null and  m.[ Category] = 'C49' then 99
+when tf.FNOIND IS NULL and m.[ Category] ='D'  then 100
+when tf.FNOIND IS NULL and m.[ Category] ='E'  then 100
+else f.FNOMAR
 end as 'MAXIUMVALUE',--=ROUNDUP(MAX(X1,IF(A1="IND",25,IF(A1="FNO",30,38)),AB1),0)
---Rtrim(m.[ Category]) as 'NewCAT',
 bs.ScripCode as 'BSECode',
 bs.ScripId as 'scripNameBSE',
 im.I_M,--for a,b,q ( var+elm+5) , for C,D,E(var + elm+Addi+5)
 case when sd.F23 in (11,12,13,14,15,16,20,21,22,33) then sd.F23 else ''end as 'ASM FLAG'
 into  #temp1
 from varvalues v inner join fnovalues f on v.[ scripcode] = f.[ scripcode]
-inner join maxvalue mx on v.[ scripcode] = mx.[ scripcode]
-inner join imvalue im on v.[ scripcode] = im.[ scripcode]
-inner join maxcatnew mxc on v.[ scripcode] = mxc.[ scripcode]
---inner join AVERAGEs avgs on v.[ scripcode] = avgs.[ scripcode]
 inner join  marginreport m on v.[ scripcode]  = m.[ scripcode]
+inner join maxvalue mx on m.[ scripcode] = mx.[ scripcode]
+inner join imvalue im on m.[ scripcode] = im.[ scripcode]
+inner join maxcatnew mxc on m.[ scripcode] = mxc.[ scripcode]
 left     join tokeninfo tf on m.[ scripcode] = tf.TOKEN 
 inner  join [dbo].[NSEM] ns on m.[ scripcode] = ns.Token
 left  join  [catM] ca on m.[ Category] = ca.Category
@@ -211,7 +226,7 @@ where ns.series not in ('BE','BO','BZ','BL','BT','IT','SM','SG','ST','SZ') and
 vr.series not in ('BE','BO','BL','BZ','BT','IT','SM','SG','ST','SZ') 
 group by tf.FNOIND,m.[ Category1],m.[ scripcode],m.ISINNO,m.[ Scrip_Name],ns.Series,b1.TOTTRDQTY,m.[ Category]
 ,b2.TOTTRDQTY,b3.TOTTRDQTY,
-b4.TOTTRDQTY,b5.TOTTRDQTY,b3.[CLOSE],b4.[CLOSE],b5.[CLOSE],vr.VAR,ELM,ADDI,sp.[SPANMgn%] ,
+b4.TOTTRDQTY,b5.TOTTRDQTY,b3.[CLOSE],b4.[CLOSE],b5.[CLOSE],vr.VAR,vr.ELM,vr.ADDI,sp.[SPANMgn%] ,
 sp.[ExpMgn%],ca.Margin,
 sp.[AddExpMgn%],bs.ScripCode,bs.ScripId,v.[var+el+adii],f.FNOMAR,mx.maxvalues,im.I_M,sd.F23
 order by  tf.FNOIND desc ;
@@ -223,18 +238,17 @@ select t.scripcode ,
 case when FNOIND = 'IND' and ceiling(MAXIUMVALUE) between 15 and 99 then CAT
 when FNOIND = 'FNO' and ceiling(MAXIUMVALUE) between 25 and 99 then CAT
 when FNOIND Is null  and ceiling(MAXIUMVALUE) between 33 and 99 then CAT
-when FNOIND is null  and t.CAT not in ('A','B','Q') and t.CAT not like 'A%'
-and t.CAT not like 'B%' and t.CAT not like 'Q%' and c.Base = 'C'
-and ceiling(MAXIUMVALUE) between  50 and 99 then  CAT
-when FNOIND is null  and t.CAT not in ('A','B','Q') and t.CAT not like 'A%'
-and t.CAT not like 'B%' and t.CAT not like 'Q%' and d.Base = 'D'
-and ceiling(MAXIUMVALUE) between  100 and 115 then  CAT
-when FNOIND is null  and t.CAT not in ('A','B','Q') and t.CAT not like 'A%'
-and t.CAT not like 'B%' and t.CAT not like 'Q%' and e.Base ='E'
-and ceiling(MAXIUMVALUE) = 100  then CAT
+when FNOIND is null  and (t.CAT not in ('A','B','Q') OR t.CAT not like 'A%'
+OR t.CAT not like 'B%' OR t.CAT not like 'Q%' and c.Base = 'C'
+and ceiling(MAXIUMVALUE) between  50 and 99) then  CAT
+when FNOIND is null  and (t.CAT not in ('A','B','Q') OR t.CAT not like 'A%'
+OR t.CAT not like 'B%' OR t.CAT not like 'Q%' and d.Base = 'D'
+and ceiling(MAXIUMVALUE) between  100 and 115) then  CAT
+when FNOIND is null  and (t.CAT not in ('A','B','Q') OR t.CAT not like 'A%'
+OR t.CAT not like 'B%' OR t.CAT not like 'Q%' and e.Base ='E'
+and ceiling(MAXIUMVALUE) = 100 ) then CAT
 else CAT
 end as 'NEWCAT'
-
 from #temp1 t left join catc c on t.MAXIUMVALUE = c.Margin
 left join catd d on t.MAXIUMVALUE = d.Margin
 left join cate e on t.MAXIUMVALUE = e.Margin
@@ -246,41 +260,38 @@ left join cate q on t.MAXIUMVALUE = q.Margin
 
 CTE1 as (
 
- select t.scripcode , 
- 
- case when t.FNOIND ='IND' then 20 
- else 25 
- end as 'mxc'
- from #temp1 t 
+select t.scripcode , 
+case when t.FNOIND ='IND' then 20 
+else 25 
+end as 'mxc'
+from #temp1 t 
  ),
 
- CTE2 as (
+CTE2 as (
 
- Select t1.scripcode,
-  case when c1.mxc > I_M then c1.mxc
- when I_M > c1.mxc then I_M
- else 25 
-
+Select t1.scripcode,
+case when c1.mxc > I_M then c1.mxc
+when I_M > c1.mxc then I_M
+else 25 
 end as 'MAXCAT'
-  from #temp1 t1 inner join CTE1 c1 on t1.scripcode = c1.scripcode
+from #temp1 t1 inner join CTE1 c1 on t1.scripcode = c1.scripcode
  ),
 
- CTE3 as (
- 
- Select    t.scripcode,
- 
- case when FNOIND = 'IND' and ceiling(MAXCAT) between 15 and 99 then CAT_1
+CTE3 as (
+
+Select    t.scripcode,
+case when FNOIND = 'IND' and ceiling(MAXCAT) between 15 and 99 then CAT_1
 when FNOIND = 'FNO' and ceiling(MAXCAT) between 25 and 99 then CAT_1
 when FNOIND = NULL and ceiling(MAXCAT) between 33 and 99 then CAT_1
-when FNOIND is null  and t.CAT not in ('A','B','Q') and t.CAT not like 'A%'
-and t.CAT not like 'B%' and t.CAT not like 'Q%' and c.Base = 'C'
-and ceiling(MAXCAT) between  50 and 99 then  CAT_1
-when FNOIND is null  and t.CAT not in ('A','B','Q') and t.CAT not like 'A%'
-and t.CAT not like 'B%' and t.CAT not like 'Q%' and d.Base = 'D'
-and ceiling(MAXCAT) between  100 and 115 then  CAT_1
-when FNOIND is null  and t.CAT not in ('A','B','Q') and t.CAT not like 'A%'
-and t.CAT not like 'B%' and t.CAT not like 'Q%' and e.Base ='E'
-and ceiling(MAXCAT) = 100  then  CAT_1
+when FNOIND is null  and (t.CAT not in ('A','B','Q') OR t.CAT not like 'A%'
+OR t.CAT not like 'B%' OR t.CAT not like 'Q%' and c.Base = 'C'
+and ceiling(MAXCAT) between  50 and 99 )then  CAT_1
+when FNOIND is null  and (t.CAT not in ('A','B','Q') OR t.CAT not like 'A%'
+OR t.CAT not like 'B%' OR t.CAT not like 'Q%' and d.Base = 'D'
+and ceiling(MAXCAT) between  100 and 115 )then  CAT_1
+when FNOIND is null  and (t.CAT not in ('A','B','Q') OR t.CAT not like 'A%'
+OR t.CAT not like 'B%' OR t.CAT not like 'Q%' and e.Base ='E'
+and ceiling(MAXCAT) = 100)  then  CAT_1
 else CAT_1
 end as 'CAT1NEW'
 from #temp1 t inner join CTE2 c2 on t.scripcode = c2.scripcode
@@ -289,9 +300,8 @@ left join catd d on c2.MAXCAT = d.Margin
 left join cate e on c2.MAXCAT = e.Margin
 left join cate a on c2.MAXCAT = a.Margin
 left join cate b on c2.MAXCAT = b.Margin
-left join cate q on c2.MAXCAT = q.Margin
- 
- )
+left join cate q on c2.MAXCAT = q.Margin 
+)
 
 select  
 FNOIND,CAT_1,ISINNO,t.scripcode,[Symbol Series],Series,CAT,BASE,[T-4V],[T-3V],[T-2V],[T-1V],[T]
@@ -299,22 +309,25 @@ FNOIND,CAT_1,ISINNO,t.scripcode,[Symbol Series],Series,CAT,BASE,[T-4V],[T-3V],[T
 ISNULL ([SPANMgn%],0) as [SPANMgn%]
 ,ISNULL ([ExpMgn%],0) as [ExpMgn%]
 ,ISNULL ([AddExpMgn%],0) as [AddExpMgn%]
-,isnull (FNOMAR,0) as FNOMAR
-,ceiling(MAXIUMVALUE) as MAXIMUMVALUE,
-c.NEWCAT,
-t.BSECode,t.scripNameBSE
+,isnull (FNOMAR,0) as FNOMAR,
+Case when MAXIUMVALUE > 100 then 100 
+else ceiling(MAXIUMVALUE)
+end as 'MAXIUMVALUE'
+,c.NEWCAT
+,t.BSECode
+,t.scripNameBSE
 ,I_M,
-ceiling(c2.MAXCAT) as MAXCAT,
-c3.CAT1NEW,
-[ASM FLAG]
-
+Case when c2.MAXCAT > 100 then 100 
+else ceiling(c2.MAXCAT)
+end as 'MAXCAT'
+,c3.CAT1NEW
+,[ASM FLAG]
+--into #final
 from 
 #temp1 t inner join CTE2 c2 on t.scripcode = c2.scripcode
 inner join CTE c on t.scripcode = c.scripcode
 inner join CTE3 c3 on t.scripcode = c3.scripcode
-where 
-rownn   = 1  
---and ISINNO = 'INE776C01039'
+where rownn   = 1  --and ISINNO = 'IN0020160043'
 group by FNOIND,CAT_1,ISINNO,t.scripcode,[Symbol Series],Series,CAT,BASE,[T-4V],[T-3V],[T-2V],[T-1V],[T]
 ,[T-2Rate],[T-1Rate],[TRate],[1Day],[2Day],[AVERAGES ],[VAR],ELM,ADDI,[var+el+adii],
 [SPANMgn%]
@@ -325,15 +338,23 @@ t.BSECode,t.scripNameBSE
 c2.MAXCAT,
 c3.CAT1NEW,
 [ASM FLAG]
-
 order by FNOIND desc
 
 
 
-select * from #temp1
 
 
+----select [VAR],ELM,ADDI,[var+el+adii],MAXIMUMVALUE,* from #final where CAT = 'D'  or CAT like 'C%'
 
+--select  f.FNOIND,f.FNOMAR,f.[var+el+adii],f.CAT,f.MAXIMUMVALUE ,[FNO Mar],m.[VAR+XEML+Addi],m.Max ,f.ISINNO,f.ISINNO from #final f inner join mainsct m on 
+--f.ISINNO = m.ISINNO
+--where    f.MAXIMUMVALUE <> m.[Max]
+
+
+--select if(c2.MAXCAT>100,c2.MAXCAT,c2.MAXCAT) from #final where  ISINNO ='INE253B01015'
+--and f.scripcode = 1624
+
+--select * from mainsct
 --=ROUNDUP(MAX(X1,IF(A1="IND",25,IF(A1="FNO",30,38)),AB1),0)
 
 --x1 = var+adii+elm
@@ -385,6 +406,3 @@ select * from #temp1
 --select top 1  * from marginreport order  by 1 
 
 --select top 100 *  from tokeninfo
-
- 
-
